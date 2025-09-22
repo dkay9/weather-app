f0456bfe387f9c70d40ccd4439c4a99fe3b86dac
Dkay <kaludavid411@gmail.com>
2025-09-22 15:42:03 +0100

    new push

--- weather-app/src/components/SearchBar.jsx
+++ weather-app/src/components/SearchBar.jsx
@@ -2,12 +2,21 @@ import SearchIcon from '../assets/images/icon-search.svg';
 
 export default function SeachBar() {
     return (
-        <div className='flex flex-col items-center mt-8 px-8'>
-            <div className='flex items-center gap-3 bg-neutral-600 border border-neutral-700 rounded-md px-4 py-2 max-w-md mx-auto mt-6 mb-4 w-full'>
-                <img src={SearchIcon} alt="Search Icon " />
-                <input className='bg-transparent' type="text" placeholder='Search for a place...' />
+        <div className="flex flex-col md:flex-row items-center justify-center mt-10 px-8 gap-4 ">
+            {/* Search Bar */}
+            <div className="flex items-center gap-6 bg-neutral-600 border border-neutral-700 rounded-md px-4 py-2 mt-6 md:mt-0 w-full md:w-96">
+                <img src={SearchIcon} alt="Search Icon" />
+                <input
+                className="bg-transparent focus:outline-none flex-1"
+                type="text"
+                placeholder="Search for a place..."
+                />
             </div>
-            <button className='px-5 py-2 bg-blue-500 rounded-md w-full'>Search</button>
+
+            {/* Search Button */}
+            <button className="px-5 py-2 bg-blue-500 hover:bg-blue-700 rounded-md w-full md:w-28 text-base focus:outline-none focus:ring">
+                Search
+            </button>
         </div>
     )
 }
