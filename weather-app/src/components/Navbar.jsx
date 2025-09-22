4065288e97e1de8364a5f1078e4891c590131cca
Dkay <kaludvaid411@gmail.com>
2025-09-22 15:11:48 +0100

    navbar fix

--- weather-app/src/components/Navbar.jsx
+++ weather-app/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import UnitDropdown from './UnitDropdown';
 
 export default function Navbar() {
     return (
-        <div className='flex items-center justify-between px-12 py-4 bg-transparent text-white'>
-            <img src={logo} alt="Logo" />
+        <div className='flex items-center justify-between w-full px-4 md:px-12 py-4 bg-transparent text-white'>
+            <img className='w-44' src={logo} alt="Logo" />
             <UnitDropdown />
         </div>
     )
--- weather-app/src/components/SearchBar.jsx
+++ weather-app/src/components/SearchBar.jsx
@@ -2,12 +2,12 @@ import SearchIcon from '../assets/images/icon-search.svg';
 
 export default function SeachBar() {
     return (
-        <div >
-            <div className='flex items-center gap-3 border border-neutral-700 rounded-md px-4 py-2 max-w-md mx-auto mt-6 mb-4'>
+        <div className='flex flex-col items-center mt-8 px-8'>
+            <div className='flex items-center gap-3 bg-neutral-600 border border-neutral-700 rounded-md px-4 py-2 max-w-md mx-auto mt-6 mb-4 w-full'>
                 <img src={SearchIcon} alt="Search Icon " />
-                <input className='bg-transparent' type="text" placeholder='Search for a place' />
+                <input className='bg-transparent' type="text" placeholder='Search for a place...' />
             </div>
-            <button className='px-5 py-2 bg-blue-500 rounded-md'>Search</button>
+            <button className='px-5 py-2 bg-blue-500 rounded-md w-full'>Search</button>
         </div>
     )
 }
