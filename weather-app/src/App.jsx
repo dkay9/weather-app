f0c484c6a61f1c70c55644b12ca078103048c7d7
Dkay <kaludavid411@gmail.com>
2025-09-22 15:11:48 +0100

    navbar fix

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className='min-h-screen bg-neutral-900 text-white'>
       <Navbar />
-      <h1 className='font-bricola text-4xl text-center mt-5'>How's the sky looking today?</h1>
+      <h1 className='font-bricola text-6xl text-center mt-5 px-3'>How's the sky looking today?</h1>
       <SeachBar />
     </div>
   )
